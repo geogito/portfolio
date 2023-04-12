472ea884add58283fc6718ed37108bbd54289f3f
Geogito <geoffreygit@gmail.com>
2023-04-12 15:11:17 +0300

    my portfolio

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html,Head,Main,NextScript} from 'next/document';
+export default function Document() {
+    return(
+        <html className='dark'>
+            <Head>
+
+            </Head>
+            <body>
+                <main/>
+                    <NextScript/>
+                
+            </body>
+
+
+        </html>
+    )
+}
